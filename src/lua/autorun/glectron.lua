2a67f79bbc1a5bbbdc26b1a730548c0271da6193
YUCLing <luotianyi@luotianyi.me>
2023-06-03 02:57:02 +0800

    chore: better client loader logic

--- src/lua/autorun/glectron.lua
+++ src/lua/autorun/glectron.lua
@@ -10,69 +10,72 @@ local GTRON_ATTRIBUTES = { -- DO NOT MODIFY
 }
 local LOADER_VER = 1
 
-_GTRON_VERSIONS = _GTRON_VERSIONS or {}
-
-local function checkAndInitialize()
-    print(string.format("Found %d Glectron version(s)", #_GTRON_VERSIONS))
-    table.sort(_GTRON_VERSIONS, function(a, b)
-        local AVER = a.VERSION
-        local BVER = b.VERSION
-        
-        if AVER.MAJOR > BVER.MAJOR then
-            return true
-        elseif AVER.MAJOR < BVER.MAJOR then
-            return false
-        end
-
-        if AVER.MINOR > BVER.MINOR then
-            return true
-        elseif AVER.MINOR < BVER.MINOR then
-            return false
-        end
-
-        if AVER.PATCH > BVER.PATCH then
-            return true
-        elseif AVER.PATCH < BVER.PATCH then
-            return false
-        end
-
-        return not a.EMBED and b.EMBED
-    end)
-
-    local bestVersion = _GTRON_VERSIONS[1]
-    local ver = bestVersion.VERSION
-
-    GLECTRON_PATH = bestVersion.PATH
-
-    if SERVER then
-        local function addDirectory(dir)
-            local files, dirs = file.Find(dir .. "/*", "LUA")
-            for _,v in pairs(files) do
-                if string.EndsWith(v, ".lua") then
-                    AddCSLuaFile(dir .. "/" .. v)
-                end
-            end
-            for _,v in pairs(dirs) do
-                addDirectory(dir .. "/" .. v)
+if SERVER then
+    local function addDirectory(dir)
+        local files, dirs = file.Find(dir .. "/*", "LUA")
+        for _,v in pairs(files) do
+            if string.EndsWith(v, ".lua") then
+                AddCSLuaFile(dir .. "/" .. v)
             end
         end
-    
-        addDirectory(bestVersion.PATH)
-        print(string.format("Using Glectron v%d.%d.%d (%s Version) in clients", ver.MAJOR, ver.MINOR, ver.PATCH, bestVersion.EMBED and "Embeded" or "Standalone"))
-        return
+        for _,v in pairs(dirs) do
+            addDirectory(dir .. "/" .. v)
+        end
     end
 
-    print(string.format("Initializing Glectron v%d.%d.%d (%s Version)", ver.MAJOR, ver.MINOR, ver.PATCH, bestVersion.EMBED and "Embeded" or "Standalone"))
-    include(bestVersion.PATH .. "/init.lua")
+    _GTRON_VERSIONS = _GTRON_VERSIONS or {}
+
+    local function checkAndInitialize()
+        print(string.format("Found %d Glectron version(s)", #_GTRON_VERSIONS))
+        table.sort(_GTRON_VERSIONS, function(a, b)
+            local AVER = a.VERSION
+            local BVER = b.VERSION
+            
+            if AVER.MAJOR > BVER.MAJOR then
+                return true
+            elseif AVER.MAJOR < BVER.MAJOR then
+                return false
+            end
+
+            if AVER.MINOR > BVER.MINOR then
+                return true
+            elseif AVER.MINOR < BVER.MINOR then
+                return false
+            end
+
+            if AVER.PATCH > BVER.PATCH then
+                return true
+            elseif AVER.PATCH < BVER.PATCH then
+                return false
+            end
+
+            return not a.EMBED and b.EMBED
+        end)
+
+        local bestVersion = _GTRON_VERSIONS[1]
+        local ver = bestVersion.VERSION
+
+        GLECTRON_PATH = bestVersion.PATH
+        
+        addDirectory(bestVersion.PATH)
+        print(string.format("Using Glectron v%d.%d.%d (%s Version) in clients", ver.MAJOR, ver.MINOR, ver.PATCH, bestVersion.EMBED and "Embeded" or "Standalone"))
+    end
+
+    local function registerGlectronVersion(attribute)
+        table.insert(_GTRON_VERSIONS, attribute)
+    end
+
+    registerGlectronVersion(GTRON_ATTRIBUTES)
+    if not GTRON_LOADER_VER or GTRON_LOADER_VER < LOADER_VER then
+        hook.Add("Initialize", "GlectronLoader", checkAndInitialize)
+        GTRON_LOADER_VER = LOADER_VER
+    end
+end
+
+if CLIENT and file.Exists(GTRON_PATH .. "/init.lua", "LUA") then
+    local ver = GTRON_ATTRIBUTES.VERSION
+    GLECTRON_PATH = GTRON_PATH
+    print(string.format("Initializing Glectron v%d.%d.%d (%s Version)", ver.MAJOR, ver.MINOR, ver.PATCH, GTRON_ATTRIBUTES.EMBED and "Embeded" or "Standalone"))
+    include(GTRON_PATH .. "/init.lua")
     print("Glectron is loaded.")
-end
-
-local function registerGlectronVersion(attribute)
-    table.insert(_GTRON_VERSIONS, attribute)
-end
-
-registerGlectronVersion(GTRON_ATTRIBUTES)
-if not GTRON_LOADER_VER or GTRON_LOADER_VER < LOADER_VER then
-    hook.Add("Initialize", "GlectronLoader", checkAndInitialize)
-    GTRON_LOADER_VER = LOADER_VER
 end
