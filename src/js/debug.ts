57fb6c6c0185295e7ca1d7afa64c2cc4964a6fe7
YUCLing <luotianyi@luotianyi.me>
2023-06-07 14:22:16 +0800

    chore: new debug mode app indicator

--- src/js/debug.ts
+++ src/js/debug.ts
@@ -6,7 +6,17 @@ const interop = {
 function initialize(...args: unknown[]) {
     interop.wrappers = args[0];
     interop.objects = args[1];
-    document.title = "_GLECTRON_APP_";
+    document.title = (document.title == "" ? location.href : document.title) + "\u2061";
+    const observer = new ("MutationObserver" in window ? MutationObserver : window["WebKitMutationObserver"])((mutation) => {
+        const newVal = mutation[0].target.nodeValue;
+        if (newVal.indexOf("\u2061") != -1) return;
+        document.title = newVal + "\u2061";
+    });
+    observer.observe(document.querySelector("title"), {
+        subtree: true,
+        characterData: true,
+        childList: true
+    });
     console.log("Glectron library is running in debug mode.");
 }
 
